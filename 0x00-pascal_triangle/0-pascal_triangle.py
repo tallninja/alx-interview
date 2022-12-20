01125aae3c2746b6a40d66083f5c7b1ff08952f4
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-20 05:56:42 +0300

    0-pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle.py
+"""
+
+
+def pascal_triangle(n):
+    triangle = []  # create an empty triangle
+
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = [1]
+            for j in range(i - 1):
+                row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+            row.append(1)
+            triangle.append(row)
+
+    return triangle

6cbbffe00fee0cbed969d28953dcc579ec200468
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-05 18:10:23 +0300

    0-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a method that determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """function to check if key exists"""
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
